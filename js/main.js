cca78379450f8fef95bcc996c50dd5abf299c292
Antonella Cino <antonella.cino18@gmail.com>
2020-03-31 15:35:43 +0200

    inizio es

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//primo click, appare il menù;
+//se cliccko che è già visibile, invece scompare.
+
+
+console.log("CIAO");
+
+
+var a = $(́".fa-bars");
+var stateHamburgerMenu = "hidden";
+a.click(
+  function () {
+    if (stateHamburgerMenu === hidden) {
+      $('.hamburger-menu').fadeIn();
+      stateHamburgerMenu = "visible";
+    } else {
+      $('.hamburger-menu').fadeOut();
+      stateHamburgerMenu = "hidden";
+    }
+  }
+)
